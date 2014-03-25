4bb4e28f03a23d99e0b0957d29ea943ddff1e813
sergolef <sergolef@gmail.com>
2014-03-25 03:27:40 +0200

    dayly commit
    1. added slider
    2. create contact form

--- sites/all/themes/simplecorp/page.tpl.php
+++ sites/all/themes/simplecorp/page.tpl.php
@@ -118,51 +118,7 @@
                 <!-- EOF: #banner-inside -->        
                 <?php endif; ?>
 
-                <?php if (theme_get_setting('slideshow_display','simplecorp')): ?>
 
-					<?php if ($is_front): ?>
-                    <!-- #slider-container -->
-                    <div id="slider-container">
-                        <div class="flexslider loading">
-                            <ul class="slides">
-
-                                <!-- first-slide -->
-                                <li class="slider-item">
-                                    <div class="slider-image">
-                                        <a href="<?php print base_path();?>"><img src="<?php print base_path() . drupal_get_path('theme', 'simplecorp') ;?>/images/sampleimages/img1.jpg" alt="" /></a>
-                                    </div>
-                                    <div class="flex-caption">
-                                        <h3>Quisque eu nibh enim, ac aliquam nunc.</h3>
-                                    </div>
-                                </li>
-
-                                <!-- second-slide -->
-                                <li class="slider-item">
-                                    <div class="slider-image">
-                                        <a href="<?php print base_path();?>"><img src="<?php print base_path() . drupal_get_path('theme', 'simplecorp') ;?>/images/sampleimages/img2.jpg" alt="" /></a>
-                                    </div>
-                                    <div class="flex-caption">
-                                        <h3>Quisque eu nibh enim, ac aliquam nunc.</h3>
-                                    </div>
-                                </li>
-
-                                <!-- third-slide -->
-                                <li class="slider-item">
-                                    <div class="slider-image">
-                                        <a href="<?php print base_path();?>"><img src="<?php print base_path() . drupal_get_path('theme', 'simplecorp') ;?>/images/sampleimages/img3.jpg" alt="" /></a>
-                                    </div>
-                                    <div class="flex-caption">
-                                        <h3>Quisque eu nibh enim, ac aliquam nunc.</h3>
-                                    </div>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- EOF: #slider-container -->
-                    <?php endif; ?>
-
-                <?php endif; ?>
             
             </div>
             <!-- EOF: #banner -->
